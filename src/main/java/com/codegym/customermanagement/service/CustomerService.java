ef81c46ecad613bee14a71c6e5f68cf3a82becec
thanhson003 <nthanhson0404@gmail.com>
2026-07-13 17:14:56 +0700

    first

---
+++ src/main/java/com/codegym/customermanagement/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.codegym.customermanagement.service;
+
+import com.codegym.customermanagement.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    Customer findById(int id);
+
+}
